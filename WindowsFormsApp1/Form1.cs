48fcda9b854d413364c06aef83f4233595283dc4
User <User@F602-39>
2022-10-12 10:25:39 +0800

    修改把main併入分支後的程式碼

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -22,10 +22,5 @@ namespace WindowsFormsApp1
             MessageBox.Show("Hello world!");
             MessageBox.Show("gogogo");
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("gogogo");
-        }
     }
 }
