1369ed4b390dd2a6c18840728f9da69f9cda905c
User <User@F602-39>
2022-10-12 10:12:59 +0800

    完成第一個需求，跳出Hello World

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.button1.Location = new System.Drawing.Point(230, 133);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(367, 105);
